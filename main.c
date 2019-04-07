cc66ea2083feb8ef9c5fe901c728e1234a5843cb
Ehren Rodgers <ehren.rodgers@gmail.com>
2019-04-07 04:45:33 +0000

    Filled array with alphabet using ASCII conversion

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
-int main(){
+#include <stdlib.h>
+#include <math.h>
 
-
+int main()
+{
+   int key, fill = 0; //between -25 & 25
+   char alphabet[24];
+   for(int i = 97; i<123; i++) //filling alphabet array using ASCII chars
+   {
+       char c = i;
+       alphabet[fill] = c;
+       printf("%c\n", alphabet[fill]);
+       fill += fill;
+       
+   }
 
 return 0;
 }
+
