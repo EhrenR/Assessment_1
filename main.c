dbdf963f02a4fa09af51ef16459076a54b7a53da
Ehren Rodgers <ehren.rodgers@gmail.com>
2019-04-15 06:01:03 +0000

    Added encryption and decryption functions

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 
+int eX (int a, int b);
+int dC (int a, int b);
+
 int main()
 {
-   int key, fill = 0; //between -25 & 25
-   char alphabet[24];
+   int fill = 0; //between -25 & 25
+   char alphabet[24]; //array of the alphabet
    for(int i = 97; i<123; i++) //filling alphabet array using ASCII chars
    {
        char c = i;
@@ -14,7 +17,27 @@ int main()
        fill += fill;
        
    }
-
-return 0;
+   //int key = rand()%26; //key is a random number between 0-26
+   int key = 1;
+   return 0;
 }
 
+int eX (int a, int b)
+{
+    int encryption = (a+b)*(%26);
+    if (encryption < 0)
+    {
+        encryption += 26;
+    }
+    return encryption;
+}
+
+int dC (int a, int b)
+{
+    int decryption = (a-b)*(%26);
+    if (decryption < 0)
+    {
+        decryption += 26;
+    }
+    return decryption;
+}
